49f8651661f22f1cec76f7a19d7b57184b262536
SaifOmar <aliensaif722@gmail.com>
2025-02-28 14:05:56 +0200

    feat:google-login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -29,6 +33,33 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         return redirect()->intended(route('home', absolute: false));
+    }
+    public function redirectToGoogle(): RedirectResponse
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleGoogleCallback(): RedirectResponse
+
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        if (User::where('email', $googleUser->email)->exists()) {
+            $user = User::where('email', $googleUser->email)->first();
+            Auth::login($user);
+            return redirect()->route('home');
+        }
+        $user = new User;
+        $user->name = $googleUser->name;
+        $user->email = $googleUser->email;
+        $user->password = Hash::make("$user->name$user->email");
+        $user->email_verified_at = now();
+        $user->remember_token = Str::random(60);
+        $user->save();
+
+        Auth::login($user);
+
+
+        return redirect()->route('home');
     }
 
     /**
--- routes/auth.php
+++ routes/auth.php
@@ -22,6 +22,9 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
+    // logging in with google
+    Route::get('login/google', [AuthenticatedSessionController::class, 'redirectToGoogle'])
+        ->name('login.google');
     // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
     //     ->name('password.request');
     //
